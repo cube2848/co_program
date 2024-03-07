c3bc191a66c6c91200c7eea68c8be11f5e027775
cube2848 <96979895+cube2848@users.noreply.github.com>
2024-03-07 20:55:08 +0900

    3일차

---
+++ 3일차.c
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char arr[2];
+	char change;
+	char temp[2];
+
+	printf("숫자입력 : ");
+	scanf_s("%s", &arr, 3);
+
+	//change = arr - '0';
+
+	char len = strlen(arr);
+
+	for (int i = 1; i < len; i++)
+	{
+		for (int j = 0; j < len; j++)
+		{
+			for (int k = 0; k < j; k++)
+			{
+				temp[k] = arr[j] + arr[j + 1];
+			}
+
+		}
+	}
+
+	char origin = arr[0];
+	char add = temp[1];
+
+	strcat(origin, add);
+	
+	return 0;
+}
+
+//int combine(int a, int b)
+//{
+//	int ret = a;
+//	while (b > 0)
+//	{
+//		ret *= 10;
+//		b /= 10;
+//	}
+//	return ret + b;
+//}
+//
+//int solution(int a, int b)
+//{
+//	int a_b = combine(a, b);
+//	int b_a = combine(b, a);
+//	if (a_b >= b_a)
+//	{
+//		return a_b;
+//	}
+//	else
+//	{
+//		return b_a;
+//	}
+//}
+//
+//int main()
+//{
+//	int a = 123;
+//	int b = 2;
+//	//printf("숫자입력 : "); // 스페이스 해주기
+//	//scanf_s("%d%d", &a, &b, 1);
+//	int result = solution(a, b);
+//	printf("%d", result);
+//
+//	//int temp = (10 * a) + (b * 1);
+//	
+//
+//	
+//	return 0;
+//}
---
+++ 저장용/2일차.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int x = 2;
+int y = 4;
+
+void map()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (x == j && y == i)
+			{
+				printf("K");
+				continue;
+			}
+			printf("X");
+		}
+		printf("\n");
+
+	}
+}
+
+void go_up()
+{
+	if (y - 1 < 0)
+	{
+		map();
+		return;
+	}
+	y--;
+	map();
+}
+
+
+void go_down()
+{
+	if (y + 1 > 4)
+	{
+		map();
+		return;
+	}
+	y++;
+	map();
+}
+
+void go_right()
+{
+	if (x + 1 > 4)
+	{
+		map();
+		return;
+	}
+	x++;
+	map();
+}
+
+void go_left()
+{
+	if (x - 1 < 0)
+	{
+		map();
+		return;
+	}
+	x--;
+	map();
+}
+
+void end_game()
+{
+	exit(0);
+}
+
+int main()
+{
+	char c;
+	map();
+
+	while (1)
+	{
+		printf("command : ");
+		scanf_s(" %c", &c, 1);
+
+
+		switch (c)
+		{
+		case 'w':
+			go_up();
+			break;
+		case 's':
+			go_down();
+			break;
+		case 'd':
+			go_right();
+			break;
+		case 'a':
+			go_left();
+			break;
+		case 'q':
+			end_game();
+			break;
+		default:
+			map();
+			break;
+		}
+	}
+
+	return 0;
+}
