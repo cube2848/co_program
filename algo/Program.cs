f5cf9fd827466c71a6b9ac3288dbce46128bac90
Hyunmyung Oh <linalan@kw.ac.kr>
2024-03-11 10:28:02 +0900

    5일차 해결

---
+++ algo/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace algo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N =int.Parse(System.Console.ReadLine());
+        string[] str = new string[N];
+        int sum = 0;
+        int stack = 1;
+        for (int i = 0; i<N; i++)
+        {
+            str[i] = System.Console.ReadLine();
+        }
+
+        for (int j = 0; j < N; j++)
+        {
+            sum = 0;
+            stack = 1;
+            foreach (var VARIABLE in str[j])
+            {
+                if (VARIABLE == 'O')
+                {
+                    sum = sum + stack;
+                    stack++;
+                }
+                else
+                {
+                    stack = 1;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
