69447c74cf8ef151054cf61d54f663a6d2781162
cube2848 <96979895+cube2848@users.noreply.github.com>
2024-03-13 21:12:23 +0900

    6일차

--- python_test/main.py
+++ python_test/main.py
@@ -1,21 +1,16 @@
-# 입력이 2개
-# 1. 테스트 케이스
-# 2. 정답입력
+import random
 
-N = int(input())
-st = []
-for i in range(N):
-    st.append((input()))
+for i in range(1,6):
+    print(str(i) + "번 학생",end=' ')
+    for j in range(1,6):
+        print(random.randint(1,9),end=' ')
+    print(end='\n')
 
-count = 0
-score = 0
-for i in range(N):
-    count = 0
-    score = 0
-    for j in st[i]:
-        if j == 'O':
+
+count = 0 ## 겹치는 수
+Class = 0 ## 공통되는거 찾기
+grade = 0 ## 학년
+for k in range(1,6):
+    for n in range(1,6):
+        if grade[n].Class[i] == grade[n].Class[i+1]:
             count = count + 1
-            score = score + count
-        elif j == 'X':
-            count = 0
-    print("점수 :",score)
